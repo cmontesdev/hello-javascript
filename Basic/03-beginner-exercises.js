e67ad9f5ec21438b85f0201f43457cd7ea5a283b
Cristofer Montes <dev+github@cmontes.dev>
2026-04-09 22:13:26 -0600

    feat: Add beginner and operator excercises

--- Basic/03-beginner-exercises.js
+++ Basic/03-beginner-exercises.js
@@ -4,21 +4,93 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=4733
 */
 
 // 1. Escribe un comentario en una línea
+// Este es un comentario en una línea
 
 // 2. Escribe un comentario en varias líneas
+/*
+Este es un comentario multilinea
+que se extiende por varias líneas
+*/
 
 // 3. Declara variables con valores asociados a todos los datos de tipo primitivos
+let myBoolean = true;
+// Para números enteros y decimales se utiliza el mismo tipo de dato: number
+let myNumber = 42;
+let myFloat = 3.14;
+// strings pueden ser con comillas simples, dobles o backticks
+let myStringWithDoubleQuotes = "Hello, world!";
+let myStringWithSingleQuotes = 'Hello, world!';
+let myStringWithBackticks = `Hello, world!`;
+let myNull = null;
+let myUndefined;
+let mySymbol = Symbol("mySymbol");
 
 // 4. Imprime por consola el valor de todas las variables
+console.log(myBoolean);
+console.log(myNumber);
+console.log(myFloat);
+console.log(myStringWithDoubleQuotes);
+console.log(myStringWithSingleQuotes);
+console.log(myStringWithBackticks);
+console.log(myNull);
+console.log(myUndefined);
+console.log(mySymbol);
 
 // 5. Imprime por consola el tipo de todas las variables
+console.log(typeof myBoolean)
+console.log(typeof myNumber)
+console.log(typeof myFloat)
+console.log(typeof myStringWithDoubleQuotes)
+console.log(typeof myStringWithSingleQuotes)
+console.log(typeof myStringWithBackticks)
+console.log(typeof myNull) // typeof null devuelve "object", es un error histórico de JavaScript
+console.log(typeof myUndefined)
+console.log(typeof mySymbol)
 
 // 6. A continuación, modifica los valores de las variables por otros del mismo tipo
+myBoolean = false
+myNumber = 100
+myFloat = 2.718
+myStringWithDoubleQuotes = "Goodbye, world!"
+myStringWithSingleQuotes = 'Goodbye, world!'
+myStringWithBackticks = `Goodbye, world!`
+myNull = {}
+myUndefined = undefined
+mySymbol = Symbol("anotherSymbol")
 
 // 7. A continuación, modifica los valores de las variables por otros de distinto tipo
+myBoolean = "This is a string now"
+myNumber = true
+myFloat = null
+myStringWithDoubleQuotes = 12345
+myStringWithSingleQuotes = false
+myStringWithBackticks = undefined
+myNull = 'Now I\'m a string'
+console.log(myNull) // Comprobando que escapé correctamente la comilla simple
+myUndefined = 3.14
+mySymbol = `Not a symbol anymore`
 
 // 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
+const myConstBoolean = true;
+const myConstNumber = 42;
+const myConstFloat = 3.14;
+const myConstStringWithDoubleQuotes = "Hello, world!";
+const myConstStringWithSingleQuotes = 'Hello, world!';
+const myConstStringWithBackticks = `Hello, world!`;
+const myConstNull = null;
+const myConstUndefined = undefined;
+const myConstSymbol = Symbol("myConstSymbol");
 
 // 9. A continuación, modifica los valores de las constantes
-
+/*
+myConstBoolean = false; 
+myConstNumber = 100; 
+myConstFloat = 2.718; 
+myConstStringWithDoubleQuotes = "Goodbye, world!"; 
+myConstStringWithSingleQuotes = 'Goodbye, world!'; 
+myConstStringWithBackticks = `Goodbye, world!`; 
+myConstNull = {}; 
+myConstUndefined = undefined; 
+myConstSymbol = Symbol("anotherConstSymbol"); 
+*/
 // 10. Comenta las líneas que produzcan algún tipo de error al ejecutarse
--- Basic/05-operators-exercises.js
+++ Basic/05-operators-exercises.js
@@ -4,22 +4,91 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=6458
 */
 
 // 1. Crea una variable para cada operación aritmética
+let sum = 5 + 3 // Suma
+let difference = 10 - 9 // Resta
+let product = 2 * 3 // Multiplicación
+let quotient = 10 / 2 // División
+let remainder = 10 % 7 // Módulo
+let exponentiation = 2 ** 3 // Exponenciación
 
 // 2. Crea una variable para cada tipo de operación de asignación,
 //    que haga uso de las variables utilizadas para las operaciones aritméticas
+console.log("\nResultados de las operaciones de asignación:")
+let sumAssignment = 0
+process.stdout.write(`${sumAssignment} += ${sum} = `)
+sumAssignment += sum
+console.log(sumAssignment)
+
+let differenceAssignment = 0
+process.stdout.write(`${differenceAssignment} -= ${difference} = `)
+differenceAssignment -= difference
+console.log(differenceAssignment)
+
+let productAssignment = 2
+process.stdout.write(`${productAssignment} *= ${product} = `)
+productAssignment *= product
+console.log(productAssignment)
+
+let quotientAssignment = 2
+process.stdout.write(`${quotientAssignment} /= ${quotient} = `)
+quotientAssignment /= quotient
+console.log(quotientAssignment)
+
+let remainderAssignment = 10
+process.stdout.write(`${remainderAssignment} %= ${remainder} = `)
+remainderAssignment %= remainder
+console.log(remainderAssignment)
+
+let exponentiationAssignment = 2
+process.stdout.write(`${exponentiationAssignment} **= ${exponentiation} = `)
+exponentiationAssignment **= exponentiation
+console.log(exponentiationAssignment)
+
 
 // 3. Imprime 5 comparaciones verdaderas con diferentes operadores de comparación
+console.log("Comparaciones verdaderas:")
+console.log(5 > 3) // Mayor que
+console.log(5 < 10) // Menor que
+console.log(5 >= 5) // Mayor o igual que
+console.log(5 === 5) // Igualdad por identidad (por tipo y valor) o igualdad estricta
+console.log(5 == 5) // Igualdad por valor
 
 // 4. Imprime 5 comparaciones falsas con diferentes operadores de comparación
+console.log("Comparaciones falsas:")
+console.log(5 < 3) // Menor que
+console.log(5 > 10) // Mayor que
+console.log(5 > 5) // Mayor que
+console.log(5 !== 5) // Desigualdad por identidad (por tipo y valor) o desigualdad estricta
+console.log(5 != 5) // Desigualdad por valor
+
 
 // 5. Utiliza el operador lógico and
+console.log("AND:")
+let trueVar = !("hola" == false) && 1 == true
+console.log(`${trueVar}, que es de tipo ${typeof trueVar}`)
 
 // 6. Utiliza el operador lógico or
+console.log(`OR:`)
+console.log(trueVar || false)
 
 // 7. Combina ambos operadores lógicos
+console.log(`AND combinado con OR:`)
+console.log((trueVar && false) || (5 > 3))
 
 // 8. Añade alguna negación
+console.log(`Negación:`)
+console.log(!trueVar)
 
 // 9. Utiliza el operador ternario
+let randomBoolean = !!Math.floor(Math.random() * 2) // coercion de tipos
+let result = randomBoolean ? "True" : "False"
+console.log(`${randomBoolean} (${typeof randomBoolean}) -> ${result}`)
+// Math.random() devuelve un numero decimal en el rango [0, 1), de modo que la probabilidad es 50/50
+let anotherRandomBoolean = Math.floor(Math.random() < 0.5) 
+let anotherResult = anotherRandomBoolean ? "True" : "False"
+console.log(`${anotherRandomBoolean} (${typeof anotherRandomBoolean}) -> ${anotherResult}`)
 
 // 10. Combina operadores aritméticos, de comparáción y lógicas
+console.log(`Combinación de operadores:`)
+let complexExpression = (5 + 3) > (10 - 9) && (2 * 3) <= (10 / 2)
+console.log(`(5 + 3) > (10 - 9) && (2 * 3) <= (10 / 2) -> ${complexExpression}`)
